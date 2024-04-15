31f31e441be4a49ce9bb321965885626b3d678aa
Parviz_8901 <parvizakmalov39@gmail.com>
2024-04-16 00:24:54 +0500

    Lesson Js Homework Sekundamer

--- main.js
+++ main.js
@@ -13,23 +13,53 @@ const btnStop = document.querySelector(".Stop")
 const btnClear = document.querySelector(".Clear")
 
 
+function zero(a) {
+    if (n <= 9) {
+        return (i.innerHTML = `0${n}`)
+    }else{
+        return(i.innerHTML = n)
+    }
+}
+
+
 
 let interval;
 btnStart.onclick = () => {
     interval = setInterval(() => {
         
         
-        if (i.innerHTML == 0) {
+        if (i.innerHTML > 0) {
+            i.innerHTML--
+        }else if (f.innerHTML > 0) {
             f.innerHTML--
             i.innerHTML = 59
-        }else{
-            i.innerHTML--
+        }else if (b.innerHTML > 0) {
+            b.innerHTML--
+            f.innerHTML = 59
+            i.innerHTML = 59
         }
-        
-        
-    }, 100)
-}
+        if (i.innerHTML <= 9) {
+            i.innerHTML =  `0${i.innerHTML++}`
+        }
+        if (f.innerHTML <= 9) {
+            f.innerHTML =  `0${f.innerHTML++}`
+        }
+        if (b.innerHTML <= 9) {
+            b.innerHTML =  `0${b.innerHTML++}`
+        }
 
+        btnStart.disabled = true
+        btnStop.disabled = false
+
+        a.disabled = true
+        d.disabled = true
+        e.disabled = true
+        g.disabled = true
+        h.disabled = true
+        j.disabled = true
+        
+    }, 1000)
+}
 
 btnClear.onclick = () => {
     b.innerHTML = "00"
@@ -38,17 +68,34 @@ btnClear.onclick = () => {
 }
 btnStop.onclick = () => {
     clearInterval(interval)
+    btnStop.disabled = true
+    btnStart.disabled = false
+
+    a.disabled = false
+        d.disabled = false
+        e.disabled = false
+        g.disabled = false
+        h.disabled = false
+        j.disabled = false
 }
 
 // soat
 a.onclick = () => {
     b.innerHTML++
+
+    if (b.innerHTML <= 9) {
+        b.innerHTML =  `0${b.innerHTML++}`
+    }
 }
 d.onclick = () => {
     if (b.innerHTML == 0) {
         b.innerHTML = 0
     }else{
         b.innerHTML--
+    }
+
+    if (b.innerHTML <= 0) {
+        b.innerHTML = `0${b.innerHTML--}`
     }
 }
 // soat oxiri
@@ -57,12 +104,24 @@ d.onclick = () => {
 // minut
 e.onclick = () => {
     f.innerHTML++
+    if (f.innerHTML > 59) {
+        f.innerHTML = 0
+        b.innerHTML++
+    }
+
+    if (f.innerHTML <= 9) {
+        f.innerHTML =  `0${f.innerHTML++}`
+    }
 }
 g.onclick = () => {
     if (f.innerHTML == 0) {
         f.innerHTML = 0
     }else{
         f.innerHTML--
+    }
+
+    if (f.innerHTML <= 0) {
+        f.innerHTML = `0${f.innerHTML--}`
     }
 }
 // minut oxiri
@@ -71,12 +130,25 @@ g.onclick = () => {
 // Sekund
 h.onclick = () => {
     i.innerHTML++
+    if (i.innerHTML > 59) {
+        i.innerHTML = 0
+        f.innerHTML++
+    }
+
+    if (i.innerHTML <= 9) {
+        i.innerHTML =  `0${i.innerHTML++}`
+    }
+
 }
 j.onclick = () => {
     if (i.innerHTML == 0) {
         i.innerHTML = 0
     }else{
         i.innerHTML--
+    }
+
+    if (i.innerHTML <= 0) {
+        i.innerHTML = `0${i.innerHTML--}`
     }
 }
 // Sekund oxiri
